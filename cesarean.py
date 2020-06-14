8d325a3d802226d376b04ab2ffa68901680fd4bb
Cob <adipo12345678@gmail.com>
2020-06-14 11:37:31 -0400

    Update cesarean.py

--- cesarean.py
+++ cesarean.py
@@ -1,37 +1,9 @@
 import string
-def encrypter():
-	while True:
-		shift = input("Shift? ")
-		try:
-			int(shift)
-		except:
-			print("Not an integer")
-		else:
-			shift=int(shift)
-			break
-	msg = input("Message? ")
+def encrypter(shift, msg):
 	msgChars = list(msg)
 	for x in range(len(msgChars)):
 		if msgChars[x] in string.ascii_letters:
 			msgChars[x] = chr(ord(msgChars[x])+shift)
 	msgShifted = ''.join(msgChars)
 	print(msgShifted)
-encrypter()
-def encrypterAlphabet():
-	while True:
-		shift = input("Shift? ")
-		try:
-			int(shift)
-		except:
-			print("Not an integer")
-		else:
-			shift=int(shift)
-			break
-	msg = input("Message? ")
-	msgChars = list(msg)
-	for x in range(len(msgChars)):
-		if (ord(msgChars[x]) >= ord('a') and ord(msgChars[x])<=ord('z') ) or (ord(msgChars[x])<=ord('Z') and ord(msgChars[x])>=ord('A')): 
-			msgChars[x] = chr(ord(msgChars[x])+shift)
-	msgShifted = ''.join(msgChars)
-	print(msgShifted)
-encrypterAlphabet()
+encrypter(1, 'Online Ganondorfs at 200% are stupid.')
