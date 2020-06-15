b3d46c625b87a8f055802127f47d7cd090335bac
Cob <adipo12345678@gmail.com>
2020-06-15 10:39:43 -0400

    DID LETTER LOOPIIIIIIIING

--- cesarean.py
+++ cesarean.py
@@ -3,7 +3,14 @@ def encrypter(shift, msg):
 	msgChars = list(msg)
 	for x in range(len(msgChars)):
 		if msgChars[x] in string.ascii_letters:
-			msgChars[x] = chr(ord(msgChars[x])+shift)
+			if chr(ord(msgChars[x])+shift) not in string.ascii_letters:
+				if msgChars[x] in string.ascii_lowercase:
+					lowerOLap = (ord(msgChars[x])+shift)-ord("z")
+			elif msgChars[x] in string.ascii_uppercase:
+					upperOLap = (ord(msgChars[x])+shift)-ord("Z")
+					msgChars[x] = chr(ord("A")+(upperOLap-1))
+			else:
+				msgChars[x] = chr(ord(msgChars[x])+shift)		
 	msgShifted = ''.join(msgChars)
 	return(msgShifted)
-encrypter(1, 'Online Ganondorfs at 200% are stupid.')
+encrypter(26, 'Online Ganondorfs at 120% are stupid.')
